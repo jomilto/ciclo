727d5eab10cb61089b89f02694c550c4483852aa
JLob Lap <joselobatof@gmail.com>
2020-08-15 19:40:27 -0500

    Read and delete implemented

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+// Para crear datos dummy de la clase user
+// crear el archivo factory que corresponda
+// php artisan tinker
+// factory(App\User::class,12)->create();
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::latest()->get();
+        return view('users.index',compact('users'));
+    }
+
+    public function store(Request $request)
+    {
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
+        return back();
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return back();
+    }
+}
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 mx-auto">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>&nbsp;</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($users as $user)
+                            <tr>
+                                <td>{{$user->id}}</td>
+                                <td>{{$user->name}}</td>
+                                <td>{{$user->email}}</td>
+                                <td>
+                                    <form action="{{route('users.destroy',$user)}}" method="post">
+                                        @method('DELETE')
+                                        @csrf
+                                        <input 
+                                            type="submit" 
+                                            value="Delete" 
+                                            class="btn btn-sm btn-danger"
+                                            onclick="return confirm('Want to Delete... ?')"
+                                        >
+                                    </form>
+                                </td>
+                            </tr>   
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
