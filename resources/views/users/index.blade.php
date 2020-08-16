3e06a4f36a21a49f7d0380f92197a2cebd5ea559
JLob Lap <joselobatof@gmail.com>
2020-08-15 20:12:51 -0500

    Creación de usuarios.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -16,6 +16,38 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-8 mx-auto">
+                    <div class="card border-0 shadow">
+                        <div class="card-body">
+                            @if($errors->any())
+                                <div class="alert alert-danger">
+                                    @foreach($errors->all() as $error)
+                                        - {{ $error }} <br>
+                                    @endforeach
+                                </div>
+                            @endif
+                            <form action="{{route('users.store')}}" method="post">
+                                <div class="form-row">
+                                    <div class="col-sm-3">
+                                        <input type="text" name="name" class="form-control" placeholder="Name" value="{{ old('name') }}">
+                                    </div>
+
+                                    <div class="col-sm-4">
+                                        <input type="email" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}">
+                                    </div>
+
+                                    <div class="col-sm-3">
+                                        <input type="password" name="password" class="form-control" placeholder="Password">
+                                    </div>
+
+                                    <div class="col-auto">
+                                        @csrf
+                                        <button type="submit" class="btn btn-primary">Save</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
                     <table class="table">
                         <thead>
                             <tr>
